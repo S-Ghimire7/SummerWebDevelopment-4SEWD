6ebe05f03385e200367fc409c0eddec3e482a4d3
unknown <foot123trick@gmail.com>
2026-07-14 09:12:11 +0545

    Week 3

---
+++ Week 3/todo-app/src/App.jsx
@@ -0,0 +1,13 @@
+import PageTitle from "./components/PageTitle/PageTitle";
+import TaskConmponent from "./components/TaskComponent/TaskComponent";
+
+function App() {
+  return (
+    <>
+      <PageTitle />
+      <TaskConmponent />
+    </>
+  );
+}
+
+export default App;
